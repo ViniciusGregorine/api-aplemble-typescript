1d1372e40bfd1c4e4c50d0af0f4d54165a45bf99
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-05 20:08:30 -0300

    ref: add class ErrorREST to handle new errors

---
+++ src/errors/errorRest.ts
@@ -0,0 +1,16 @@
+export class ErrorREST extends Error {
+    public response: { status: number; message: string;};
+ 
+     constructor(error: { status: number, message: string }, ...args: any) {
+        super(...args);
+        this.response = {status: error.status, message: error.message};
+    }
+ }
+
+ // helpers | errors
+ export const error = ({
+     badRequest: {
+         status: 400,
+         message: "something weong with your request"
+     }
+ })
