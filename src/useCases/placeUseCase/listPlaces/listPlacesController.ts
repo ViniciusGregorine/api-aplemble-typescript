b8002ba9258ae234dd070caf90287135fef6acd7
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-12 17:15:57 -0300

    feat: add listPlaceController

---
+++ src/useCases/placeUseCase/listPlaces/listPlacesController.ts
@@ -0,0 +1,18 @@
+import { errorHandle, HttpResponse, IController, ok } from '@/routeAdapter/HttpHandle'
+import { ListPlaceUseCase } from '@/useCases/placeUseCase/listPlaces/ListPlaceUseCase'
+
+export class ListPlaceControler implements IController{
+    constructor(
+        private listPlaceUseCase: ListPlaceUseCase
+    ){}
+
+    async handle(): Promise<HttpResponse>{
+        try {
+            const response = await this.listPlaceUseCase.listAll()
+            return ok(response)
+
+        } catch(error) {
+            return errorHandle(error)
+       }
+    }
+}
