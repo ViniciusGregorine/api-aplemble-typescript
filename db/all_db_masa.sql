c87a743e8920dcc38a97c7cfc9cd535bfcb6410c
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-04-29 18:42:24 -0300

    reaf: add more number of characters to varchar data types

--- db/all_db_masa.sql
+++ db/all_db_masa.sql
@@ -1,5 +1,5 @@
-create database if not exists masa_logic_3 ;
-use masa_logic_3;
+create database if not exists masa_building;
+use masa_building;
 
 create table if not exists readings (
 	id int not null  auto_increment unique,
@@ -15,7 +15,7 @@ create table if not exists readings (
 
 create table if not exists sensors (
 	id tinyint not null auto_increment unique,
-	description varchar(40) not null unique,
+	description varchar(50) not null unique,
 	installation_date date default now(),
 	device bool default 0,
 	id_situation tinyint not null,
@@ -33,7 +33,7 @@ create table if not exists gaps (
 
 create table if not exists situations (
 	id tinyint not null auto_increment unique,
-    description varchar(30) not null unique,
+    description varchar(40) not null unique,
 
 	primary key(id)
 );
@@ -42,8 +42,8 @@ create table  if not exists places (
 	id tinyint not null auto_increment unique,
     sensor_temp bool not null default true ,
     sensor_humi bool not null default true,
-    description  varchar(25) not null unique,
-    note varchar(25),
+    description  varchar(40) not null unique,
+    note varchar(200),
     lim_temperature decimal(5, 2),
     id_dimension tinyint not null,
     id_material tinyint not null,
@@ -66,17 +66,17 @@ create table if not exists dimensions (
 
 create table if not exists institutions (
 	id tinyint not null primary key auto_increment unique,
-  name varchar(50) not null unique,
-  email varchar(35) not null unique,
-  password varchar(25) not null,
-  id_adress tinyint not null
+  name varchar(60) not null unique,
+  email varchar(60) not null unique,
+  password varchar(60) not null,
+  id_address tinyint not null
 );
 
-create table if not exists adresses (
+create table if not exists addresses (
 	id tinyint not null primary key auto_increment unique,
-    street varchar(35) not null,
-    number varchar(5) not null,
-    neighborhood varchar(25) not null,
+    street varchar(40) not null,
+    number varchar(6) not null,
+    neighborhood varchar(40) not null,
     complement varchar(50),
     city varchar(40) not null default "Sombrio"
 );
@@ -103,5 +103,5 @@ ADD CONSTRAINT fk_instituition
 FOREIGN KEY (id_institution) REFERENCES institutions(id);
 
 ALTER TABLE institutions
-ADD CONSTRAINT fk_adress
-FOREIGN KEY (id_adress) REFERENCES adresses(id);
+ADD CONSTRAINT fk_address
+FOREIGN KEY (id_address) REFERENCES addresses(id);
