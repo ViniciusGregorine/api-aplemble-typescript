fbc4fb9bbdda472b59bb6df7f43ce13e4bc23aa5
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-06-29 22:30:44 -0300

    add: add I reading DTO

---
+++ src/useCases/readingUseCase/insertReading/IReadingDTO.ts
@@ -0,0 +1,10 @@
+export interface IReadingDOT {
+    id?: number
+
+    date?: string
+    hour?: number
+    value_humidity?: number
+    value_temperature?: number
+    id_place: number
+    id_sensor: number
+}
