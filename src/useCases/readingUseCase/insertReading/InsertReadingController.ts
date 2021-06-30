0f9bb1d49f294d5dd0bfde0deb8844445074cdf3
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-06-30 15:44:31 -0300

    add: adding insertReadingController.ts

---
+++ src/useCases/readingUseCase/insertReading/InsertReadingController.ts
@@ -0,0 +1,26 @@
+import { errorHandle, HttpRequest, HttpResponse, ok} from "@/routeAdapter/HttpHandle"
+import { InsertReadingUseCase } from "./InsertReadingUseCase"
+
+export class InsertReadingController {
+    constructor(
+        private insertReadingUseCase: InsertReadingUseCase 
+    ){}
+
+    async handle(req: HttpRequest): Promise<HttpResponse>{
+        if (!req.body) return errorHandle({message: 'content cannot be ampty', status: 400})
+        
+         const requiredParans = ['id_place', 'id_sensor']
+
+        for(const param of requiredParans){
+            if(!req.body[param]) return errorHandle({message: `missing param: ${param}`, status: 400})
+            }
+
+        try {
+            await this.insertReadingUseCase.execute(req.body)
+            return ok('sucessifuly sensor created')
+
+        } catch (error) {
+            return errorHandle(error)
+        }
+    }
+}
