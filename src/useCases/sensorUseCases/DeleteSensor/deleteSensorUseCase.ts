7b110addd6b456527dc96a97805f9715d33e3107
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-09-29 17:34:58 -0300

    feat: add deleteUseCase.ts

---
+++ src/useCases/sensorUseCases/DeleteSensor/deleteSensorUseCase.ts
@@ -0,0 +1,16 @@
+import { ISensorsRepository } from '@/infra/repositories/contracts/ISensorsRepository'
+import { IDeleteSensorDTO } from './IDeleteSensorDTO'
+
+export class DeleteSensorUseCase{
+    constructor(
+        private sensorsRepository: ISensorsRepository
+    ){}
+
+    async execute(data: IDeleteSensorDTO){
+        // const sensorAlreadyExist = await this.sensorsRepository.findByDescription(data.description)
+    
+        // if(!sensorAlreadyExist)  throw new Error
+
+        await this.sensorsRepository.deleteSensor(data.id)
+    }
+}
