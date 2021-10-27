037a8acac3b5971a68f9ca5f352922d4880258a7
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-10-27 06:29:51 -0300

    feat: add masa db 3

---
+++ db/all_db_masa_3.sql
@@ -0,0 +1,109 @@
+-- version with new table: `type_readings` 
+
+create database if not exists masa_building3;
+use masa_building3;
+
+create table if not exists readings (
+	id int not null  auto_increment unique,
+    date date not null default(now()),
+    hour time not null default now(),
+
+    value decimal(6, 3) not null,
+    
+    id_type_reading int not null,
+    id_place tinyint not null,
+    id_sensor tinyint,
+    
+    primary key(id)
+);
+
+create table if not exists type_readings (
+	id int not null auto_increment unique,
+    description varchar(50) not null unique,
+    min_value decimal(5, 2),
+    max_value decimal(5, 2),
+    prefix varchar(10),
+    
+    primary key(id)
+);
+
+create table if not exists sensors (
+	id tinyint not null auto_increment unique,
+	description varchar(50) not null unique,
+	installation_date date default now(),
+	device bool default 0,
+	id_situation tinyint not null,
+	id_gap int,
+    
+    primary key(id)
+);
+
+create table if not exists gaps (
+	id int not null  auto_increment unique,
+	description int not null unique,
+    
+    primary key(id)
+);
+
+create table if not exists situations (
+	id tinyint not null auto_increment unique,
+    description varchar(40) not null unique,
+
+	primary key(id)
+);
+
+create table  if not exists places (
+	id tinyint not null auto_increment unique,
+    sensor_temp bool not null default true ,
+    sensor_humi bool not null default true,
+    description  varchar(40) not null unique,
+    note varchar(200),
+    lim_temperature decimal(5, 2),
+    id_dimension tinyint not null,
+    id_material tinyint not null,
+    id_user tinyint default 1 not null, 
+    
+    primary key (id)
+);
+
+create table if not exists materials (
+	id tinyint not null primary key auto_increment unique,
+	description varchar(25) not null unique
+);
+
+create table if not exists dimensions (
+	id tinyint not null primary key auto_increment unique,
+	height decimal(5, 2) not null,
+	width decimal(5, 2) not null,
+	length decimal(5, 2) not null
+);
+
+
+create table if not exists users (
+    id tinyint not null primary key auto_increment unique, 
+    email varchar(60) not null unique,
+    password varchar(60) not null
+);
+-- fks 
+ALTER TABLE  sensors
+ADD CONSTRAINT fk_situation
+FOREIGN KEY (id_situation) REFERENCES situations(id),
+ADD CONSTRAINT fk_gap
+FOREIGN KEY (id_gap) REFERENCES gaps(id);
+
+ALTER TABLE readings
+ADD CONSTRAINT fk_place
+FOREIGN KEY (id_place) REFERENCES places(id),
+ADD CONSTRAINT fk_sensor
+FOREIGN KEY (id_sensor) REFERENCES	sensors(id) ON DELETE SET NULL,
+
+ADD CONSTRAINT fk_type_reading 
+FOREIGN KEy (id_type_reading) REFERENCES type_readings(id);
+
+ALTER TABLE places
+ADD CONSTRAINT fk_dimension
+FOREIGN KEY (id_dimension) REFERENCES dimensions(id),
+ADD CONSTRAINT fk_material
+FOREIGN KEY (id_material) REFERENCES materials(id),
+ADD CONSTRAINT fk_user 
+FOREIGN  KEY (id_user) REFERENCES users(id)
---
+++ db/insert_exemple_sensor_3.sql
@@ -0,0 +1,32 @@
+-- INSERT INTO sensors 
+--    SET description = 'DHT11',
+--        id_situation = (
+--        SELECT id
+--          FROM situations
+--         WHERE description = 'quebrado'),
+	
+-- 		id_gap = (
+--         select id 
+--         from gaps
+--         where description = 160)
+
+INSERT INTO `gaps` (`id`, `description`) VALUES (NULL, '10');
+
+INSERT INTO `situations`(`description`) VALUES ('ligado');
+
+INSERT INTO `sensors` (`id`, `description`, `installation_date`, `device`, `id_situation`, `id_gap`) VALUES (NULL, 'DHT14', '2021-03-06', '0', '1', '1');
+
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES (NULL,'ifc@gmail.com', '1234');
+
+INSERT INTO `materials` (`id`, `description`) VALUES (NULL, 'madeira'), (NULL, 'isopor');
+
+INSERT INTO `dimensions` (`id`, `height`, `width`, `length`) VALUES (NULL, '1', '1', '1');
+
+INSERT INTO `places` (`id`, `sensor_temp`, `sensor_humi`, `description`, `note`, `lim_temperature`, `id_dimension`, `id_material`, `id_institution`) VALUES (NULL, '1', '1', 'modulo de isopor', 'localizado aqui', '100', '1', '2', '1');
+
+-- INSERT INTO `type_readings` (`id`, `description`, `min_value`, `max_value`, `prefix`) VALUES (NULL, 'temperature', '0', '100', '°C');
+
+INSERT INTO `readings` (`id`, `date`, `hour`, `value`, `id_place`, `id_sensor`, `id_type_reading`) VALUES (NULL, '2021-03-06', '09:13:11', '85', '1', '1', '1');
+
+INSERT INTO `readings` (`id`, `date`, `hour`, `value_humidity`, `value_temperature`, `id_place`, `id_sensor`) VALUES (NULL, '2021-03-06', '09:15:11', '95', '60', '1', '1');
