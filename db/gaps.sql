125a55c62d1e068a62d15cde9920d0cc016d0c07
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-10-28 16:46:30 -0300

    refactor: deleting unusual sql scripts

--- db/gaps.sql
+++
@@ -1,6 +0,0 @@
-create table gaps (
-	id int not null  auto_increment unique,
-	description int not null unique,
-    
-    primary key(id)
-)
--- db/institutions.sql
+++
@@ -1,7 +0,0 @@
-create table institutions (
-	id tinyint not null primary key auto_increment unique,
-  name varchar(50) not null unique,
-  email varchar(35) not null unique,
-  password varchar(60) not null,
-  id_adress tinyint not null
-);
