8691878c8caf94c9c620f7ef6c260a72d28ba907
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-11-18 18:48:27 -0300

    feat: add `save` method to placeRepository

--- src/infra/repositories/contracts/IPlaceRepository.ts
+++ src/infra/repositories/contracts/IPlaceRepository.ts
@@ -2,4 +2,8 @@ import {IPlace} from '@/domain/entities/IPlace'
 
 export interface IPlaceRepository {
     getAllPlace(): Promise<IPlace[]>
+
+    save(place: IPlace): Promise<void>
+    // update(place: IPlace): Promise<void>
+
 }
--- src/infra/repositories/implementation/MysqlPlaceRepository.ts
+++ src/infra/repositories/implementation/MysqlPlaceRepository.ts
@@ -2,8 +2,6 @@ import {pool} from '@/infra/repositories/implementation/poolConnection'
 import { IPlace } from '@/domain/entities/IPlace'
 import {IPlaceRepository} from '@/infra/repositories/contracts/IPlaceRepository'
 
-
-
 export class MysqlPlaceRepository implements IPlaceRepository{
   async getAllPlace(): Promise<any>{
     try{
@@ -28,4 +26,36 @@ export class MysqlPlaceRepository implements IPlaceRepository{
       
     }
   }
+  async save(place: IPlace): Promise<void>{
+        try {
+          let materials = await pool.query<any>(`SELECT materials.id FROM materials WHERE materials.description = '${place.material}'`)
+          let material = materials[0][0]
+
+          if(!material) {
+            await pool.query<any>(`INSERT INTO materials (description) VALUES ("${place.material}")`)
+            this.save(place)
+          }
+
+          const entite = {
+            description: place.description,
+            note: place.note,
+
+            sensor_temp: place.sensor_temp || 0,
+            sensor_humi: place.sensor_humi || 0,
+            lim_temperature: place.lim_demperature,
+            
+            id_material: material.id,
+            id_dimension: place.id_dimension,
+
+            id_user: 1
+          }
+
+         await pool.query(`INSERT INTO places SET ?`, entite)
+  
+        } catch (error) {
+            console.log(error)
+            throw new Error 
+        }
+      }
+  
 }
