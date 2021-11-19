579ce407fe1162c9838b9ba3bd6e4edb1dbb68b2
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-11-18 23:54:31 -0300

    hotfix: slq sintax error on `deleteRow.ts`

--- src/infra/repositories/helpers/deleteRow.ts
+++ src/infra/repositories/helpers/deleteRow.ts
@@ -2,7 +2,7 @@ import { pool } from "../implementation/poolConnection"
 
 export async function deleteFunction (table: string, description: string){
     try {
-        await pool.query(`DELETE FROM ${table} WHERE description=${description}`) 
+        await pool.query(`DELETE FROM ${table} WHERE description='${description}'`) 
     } catch (error: any) {
         console.log(error)
         throw new Error(error) 
