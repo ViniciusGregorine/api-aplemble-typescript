ce0c0d58fe41a459ae060b4497c7601f2b9f270c
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-11-19 16:51:02 -0300

    hotfix: error on return of `recursive function`

--- src/infra/repositories/helpers/foreignKeyManager.ts
+++ src/infra/repositories/helpers/foreignKeyManager.ts
@@ -1,3 +1,4 @@
+import { RowDataPacket } from "mysql"
 import { pool } from "../implementation/poolConnection"
 
 interface TableInfo {
@@ -5,31 +6,34 @@ interface TableInfo {
     tableColumns: any
 }
 
+interface rowResponse extends RowDataPacket {
+    id: number
+}
 
 export async function foreignKeyManager({targetTable, tableColumns}: TableInfo): Promise<number>{
-    try {
-        let query = ""
+    let query = ""
 
+    try {
         switch(targetTable){
             default:
                 query = `SELECT id FROM ${targetTable} WHERE ?`
                 break
             case 'dimensions':
-                query = `SELECT id FROM ${targetTable} WHERE ${tableColumns.length} AND ${tableColumns.width} AND ${tableColumns.height}`
+                query = `SELECT id FROM ${targetTable} WHERE length='${tableColumns.length}' AND width='${tableColumns.width}' AND height ='${tableColumns.height}'`
                 break
         }
 
-        const [rows] = await pool.query<any>(query, tableColumns)
-        let ResponseId = rows[0]
-
-        if(!ResponseId) {
-          await pool.query<any>(`INSERT INTO ${targetTable} SET ?`, tableColumns)
-
-          foreignKeyManager({targetTable: targetTable, tableColumns: tableColumns})
-        } 
+        const [rows] = await pool.query<rowResponse[]>(query, tableColumns)
         
-        return ResponseId.id
+        let responseId = rows[0]
+
+        if(!responseId) {
+            await pool.query(`INSERT INTO ${targetTable} SET ?`, tableColumns)
+            return (async () => await foreignKeyManager({targetTable: targetTable, tableColumns: tableColumns}))()
+        }
+
+        return responseId.id 
     }catch(error: any){
         throw new Error(error)
-    }
+    } 
 }
