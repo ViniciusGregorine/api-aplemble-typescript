7b671a5c0fe863cd771f53bf4fc1cc53885adf2b
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-11-18 23:57:11 -0300

    feat: deletePlaceController

---
+++ src/useCases/placeUseCase/deletePlace/deletePlaceController.ts
@@ -0,0 +1,27 @@
+import {ok, errorHandle, HttpRequest, HttpResponse } from '@/routeAdapter/HttpHandle'
+import { requiredParams } from '@/utils/requireParams'
+import { DeletePlaceUseCase } from './deletePlaceUseCase'
+
+
+
+export class DeletePlaceController {
+    constructor(
+        private deleteplaceUseCase: DeletePlaceUseCase
+    ){}
+
+    async handle(req: HttpRequest): Promise<HttpResponse>{
+
+        const error = requiredParams(['description'], req)
+        if(error) return errorHandle(error.response)
+
+        const placeDescription = req.query.description
+
+        try {
+            await this.deleteplaceUseCase.execute(placeDescription)
+            return ok('deleted')
+            
+        } catch (error: any) {
+            return errorHandle(error.response)
+        }
+    }
+}
