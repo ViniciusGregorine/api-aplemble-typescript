769b91e5006f7bc6bacd646493654c414f873afc
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-11-18 22:35:58 -0300

    feat: add `findByDescription` method

--- src/infra/repositories/contracts/IPlaceRepository.ts
+++ src/infra/repositories/contracts/IPlaceRepository.ts
@@ -1,9 +1,11 @@
 import {IPlace} from '@/domain/entities/IPlace'
+import { Place } from '@/domain/entities/Place';
 
 export interface IPlaceRepository {
     getAllPlace(): Promise<IPlace[]>
 
-    save(place: IPlace): Promise<void>
-    // update(place: IPlace): Promise<void>
+    save(place: Place): Promise<void>
+    findByDescription(type: string): Promise<string>
 
+    // update(place: IPlace): Promise<void>
 }
