5664d10f04c6169a759b51033034a58c879a5ee7
ViniciusGregorine <viniciusgmenegon@gmail.com>
2023-07-02 22:16:31 -0300

    feat: add first logic for batch reading on usecase

--- src/useCases/readingUseCase/insertReading/InsertReadingUseCase.ts
+++ src/useCases/readingUseCase/insertReading/InsertReadingUseCase.ts
@@ -8,8 +8,30 @@ export class InsertReadingUseCase{
     ){}
 
     async execute(data: IReadingDTO){
-        const reading = new Reading(data)
 
-        await this.readingRepository.save(reading)
+        if(data.value instanceof Array){
+            console.log('batch reading')
+
+            data.value.forEach(async reading => {
+                console.log(reading[0], reading[1])
+
+                const readingObj = new Reading(
+                   {
+                    value: reading[0],
+                    hour: reading[1],
+                    id_place:  data.id_place,
+                    id_sensor:  data.id_sensor,
+                    id_type_reading:  data.id_type_reading
+                   }
+                )
+
+                await this.readingRepository.save(readingObj)
+            })
+        }else{
+
+            const reading = new Reading(data)
+
+            await this.readingRepository.save(reading)
+        }
     }
 }
